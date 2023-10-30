5fd0b4e3d944050adb3f236813f9f08f55bc71bc
Michael <mikhzobby@gmail.com>
2023-10-30 13:21:27 +0100

    new slate

--- messaging/urls.py
+++ messaging/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index_messages, name='index_messages'),
-    path('<int:pk>', views.view_message, name='view_message')
+    path('message/<int:pk>', views.view_message, name='view_message')
 ]
