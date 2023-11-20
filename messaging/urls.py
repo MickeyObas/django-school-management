8e98dd1da107143db551bba74fed16a081679e0a
Michael <mikhzobby@gmail.com>
2023-11-20 15:22:53 +0100

    fdfd

--- messaging/urls.py
+++ messaging/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index_messages, name="index_messages"),
+    path("<str:category>", views.index_messages, name='filtered_index_messages'),
     path("message/<int:pk>", views.view_message, name="view_message"),
     path("send_message", views.send_message, name="send_message"),
     path("add_to_favourites", views.add_to_favourites, name='add_to_favourites'),
